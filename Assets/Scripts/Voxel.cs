976355d1a46c2eabcb02358082c7d337278c9976
Cody <chaplinbcody@gmail.com>
2023-07-13 10:28:40 -0400

    Finished first voxel.

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -7,14 +7,14 @@ public static class Voxel
     /// </summary>
     public static readonly Vector3[] verts = new Vector3[8]
     {
-        new Vector3(0.0f, 0.0f, 0.0f),
-        new Vector3(1.0f, 0.0f, 0.0f),
-        new Vector3(1.0f, 1.0f, 0.0f),
-        new Vector3(0.0f, 1.0f, 0.0f),
-        new Vector3(0.0f, 0.0f, 1.0f),
-        new Vector3(1.0f, 0.0f, 1.0f),
-        new Vector3(1.0f, 1.0f, 1.0f),
-        new Vector3(0.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 0.0f, 0.0f), // 0
+        new Vector3(1.0f, 0.0f, 0.0f), // 1
+        new Vector3(1.0f, 1.0f, 0.0f), // 2
+        new Vector3(0.0f, 1.0f, 0.0f), // 3
+        new Vector3(0.0f, 0.0f, 1.0f), // 4
+        new Vector3(1.0f, 0.0f, 1.0f), // 5
+        new Vector3(1.0f, 1.0f, 1.0f), // 6
+        new Vector3(0.0f, 1.0f, 1.0f), // 7
     };
 
     /// <summary>
@@ -28,17 +28,19 @@ public static class Voxel
 		{3, 7, 2, 2, 7, 6}, // top
 		{1, 5, 0, 0, 5, 4}, // bottom
 		{4, 7, 0, 0, 7, 3}, // left
-		{1, 2, 5, 5, 2, 6} // right
+		{1, 2, 5, 5, 2, 6}  // right
 	};
 
     /// <summary>
-    /// 
+    /// Stores array representing the direction UVs are created on a block face.
     /// </summary>
     public static readonly Vector2[] uvs = new Vector2[6]
     {
+        // bottom-left -> top-left -> bottom-right
         new Vector2 (0.0f, 0.0f),
         new Vector2 (0.0f, 1.0f),
         new Vector2 (1.0f, 0.0f),
+        // bottom-right -> top-left -> top-right
         new Vector2 (1.0f, 0.0f),
         new Vector2 (0.0f, 1.0f),
         new Vector2 (1.0f, 1.0f)
