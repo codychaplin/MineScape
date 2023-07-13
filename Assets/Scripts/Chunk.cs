27933d121999d810676ea5e0e5f920aba20fbee8
Cody <chaplinbcody@gmail.com>
2023-07-13 12:26:25 -0400

    Finished first chunk.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -6,25 +6,95 @@ public class Chunk : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
 
+    int vertexIndex = 0;
+    List<Vector3> vertices = new();
+    List<int> triangles = new();
+    List<Vector2> uvs = new();
+
+    bool[,,] Map = new bool[Voxel.ChunkWidth, Voxel.ChunkHeight, Voxel.ChunkWidth]; 
+
     void Start()
     {
-        int vertexIndex = 0;
-        List<Vector3> vertices = new();
-        List<int> triangles = new();
-        List<Vector2> uvs = new();
+        PopulateMap();
+        CreateChunkMesh();
+        CreateMesh();
+    }
 
-        for (int i = 0; i < 6; i++)
+    void PopulateMap()
+    {
+        for (int x = 0; x < Voxel.ChunkWidth; x++)
         {
-            for (int j = 0; j < 6; j++)
+            for (int z = 0; z < Voxel.ChunkWidth; z++)
             {
-                int triangleIndex = Voxel.tris[i, j];
-                vertices.Add(Voxel.verts[triangleIndex]);
-                triangles.Add(vertexIndex);
-                uvs.Add(Voxel.uvs[j]);
-                vertexIndex++;
+                for (int y = 0; y < Voxel.ChunkHeight; y++)
+                {
+                    Map[x, y, z] = true;
+                }
             }
         }
+    }
 
+    void CreateChunkMesh()
+    {
+        for (int x = 0; x < Voxel.ChunkWidth; x++)
+        {
+            for (int z = 0; z < Voxel.ChunkWidth; z++)
+            {
+                for (int y = 0; y < Voxel.ChunkHeight; y++)
+                {
+                    AddVoxelToChunk(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+
+    bool CheckVoxel(Vector3 pos)
+    {
+        // round coordinates down
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        // if out of bounds
+        if (x < 0 || x >= Voxel.ChunkWidth ||
+            y < 0 || y >= Voxel.ChunkHeight ||
+            z < 0 || z >= Voxel.ChunkWidth)
+            return false;
+
+        // return value in map
+        return Map[x, y, z];
+    }
+
+    void AddVoxelToChunk(Vector3 pos)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (CheckVoxel(pos + Voxel.faceCheck[i]))
+                continue;
+
+            vertices.Add(pos + Voxel.verts[Voxel.tris[i,0]]);
+            vertices.Add(pos + Voxel.verts[Voxel.tris[i,1]]);
+            vertices.Add(pos + Voxel.verts[Voxel.tris[i,2]]);
+            vertices.Add(pos + Voxel.verts[Voxel.tris[i,3]]);
+
+            uvs.Add(Voxel.uvs[0]);
+            uvs.Add(Voxel.uvs[1]);
+            uvs.Add(Voxel.uvs[2]);
+            uvs.Add(Voxel.uvs[3]);
+
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 3);
+
+            vertexIndex += 4;
+        }
+    }
+
+    void CreateMesh()
+    {
         Mesh mesh = new()
         {
             vertices = vertices.ToArray(),
--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public static class Voxel
 {
+    public static readonly int ChunkWidth = 16;
+    public static readonly int ChunkHeight = 16;
+
     /// <summary>
     /// Stores all 8 corners (vertices) of a 1x1 cube.
     /// </summary>
@@ -14,35 +17,45 @@ public static class Voxel
         new Vector3(0.0f, 0.0f, 1.0f), // 4
         new Vector3(1.0f, 0.0f, 1.0f), // 5
         new Vector3(1.0f, 1.0f, 1.0f), // 6
-        new Vector3(0.0f, 1.0f, 1.0f), // 7
+        new Vector3(0.0f, 1.0f, 1.0f)  // 7
     };
 
     /// <summary>
     /// Stores arrays containing 2 triangles that make up the faces of a cube.
     /// Uses indexes of verts to create triangles.
     /// </summary>
-    public static readonly int[,] tris = new int[6, 6]
+    public static readonly int[,] tris = new int[6, 4]
     {
-        {0, 3, 1, 1, 3, 2}, // back
-		{5, 6, 4, 4, 6, 7}, // front
-		{3, 7, 2, 2, 7, 6}, // top
-		{1, 5, 0, 0, 5, 4}, // bottom
-		{4, 7, 0, 0, 7, 3}, // left
-		{1, 2, 5, 5, 2, 6}  // right
+        {0, 3, 1, 2}, // back
+		{5, 6, 4, 7}, // front
+		{3, 7, 2, 6}, // top
+		{1, 5, 0, 4}, // bottom
+		{4, 7, 0, 3}, // left
+		{1, 2, 5, 6}  // right
 	};
 
     /// <summary>
     /// Stores array representing the direction UVs are created on a block face.
     /// </summary>
-    public static readonly Vector2[] uvs = new Vector2[6]
+    public static readonly Vector2[] uvs = new Vector2[4]
     {
         // bottom-left -> top-left -> bottom-right
         new Vector2 (0.0f, 0.0f),
         new Vector2 (0.0f, 1.0f),
         new Vector2 (1.0f, 0.0f),
-        // bottom-right -> top-left -> top-right
-        new Vector2 (1.0f, 0.0f),
-        new Vector2 (0.0f, 1.0f),
         new Vector2 (1.0f, 1.0f)
+    };
+
+    /// <summary>
+    /// Used to get surrounding voxel coordinates.
+    /// </summary>
+    public static readonly Vector3[] faceCheck = new Vector3[6]
+    {
+        new Vector3(0.0f, 0.0f, -1.0f), // back
+        new Vector3(0.0f, 0.0f, 1.0f), // front
+        new Vector3(0.0f, 1.0f, 0.0f), // top
+        new Vector3(0.0f, -1.0f, 0.0f), // bottom
+        new Vector3(-1.0f, 0.0f, 0.0f), // left
+        new Vector3(1.0f, 0.0f, 0.0f), // right
     };
 }
